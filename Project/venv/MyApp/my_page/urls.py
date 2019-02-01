0a681ce7d63122478723f54f4b8eb0e38532d438
vladwwe24 <vladwwe24@gmail.com>
2019-02-01 06:39:14 +0200

    release1.0.0

---
+++ Project/venv/MyApp/my_page/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    path('', views.post, name='myPage'),
+    url(r'course_id=\d', views.get, name='CourseData'),
+    # url(r'mod=\w', views.link_maker, name='LinkData'),
+]
